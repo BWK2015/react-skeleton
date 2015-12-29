d6ea496cfa13d276967eb5142c786873bdcc9dcc
Keir Beckett <beckettkeir@yahoo.co.uk>
2015-12-29 16:52:24 +0000

    Updated react skeleton with watchify

---
+++ src/components/List.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var ListItem = require('./ListItem');
+
+var ingredients = [{"id": 1, "text":"ham"},{"id": 2, "text":"cheese"},{"id": 3, "text":"pickle"}];
+
+var List = React.createClass({
+    render: function(){
+        var ListItems = ingredients.map(function(item){
+            return <ListItem key={item.id} ingredient={item.text} />   
+        });
+        
+        return (
+            <ul>
+                {ListItems}
+            </ul>
+            )
+    }
+});
+
+module.exports = List;
